b345838a2a7894ba169b5fe3ef7c8e320d229deb
hln <hl@bl.nu>
2014-12-17 20:26:00 +0100

    Ändrade "labels" för Clef

--- hex-music-core/src/main/java/hex/music/core/domain/Clef.java
+++ hex-music-core/src/main/java/hex/music/core/domain/Clef.java
@@ -43,11 +43,11 @@ public interface Clef extends DomainEntity {
 
         TREBLE("G-klav", "treble"),
         ALTO("C- eller Altklav)", "alto"),
-        BASS("F-klav, linje 4", "bass"),
+        BASS("Basklav", "bass"),
         TREBLE_8VA("G-klav 8va", "treble+8"),
         TREBLE_8VA_BASSO("G-klav 8va basso", "treble-8"),
-        BASS_8VA("F-klav 8va", "bass+8"),
-        BASS_8VA_BASSO("F-kla 8va basso", "bass-8"),
+        BASS_8VA("Basklav 8va", "bass+8"),
+        BASS_8VA_BASSO("Bas-kla 8va basso", "bass-8"),
         TREBLE1("G-klav, linje 1", "treble1"),
         TREBLE2("G-klav, linje 2", "treble2"),
         TREBLE3("G-klav, linje 3", "treble3"),
@@ -58,15 +58,15 @@ public interface Clef extends DomainEntity {
         ALTO3("C-klav, linje 3 (Altklav)", "alto3"),
         ALTO4("C-klav, linje 4 (Tenorklav)", "alto4"),
         ALTO5("C-klav, linje 5 (Baritonklav)", "alto5"),
-        BASS1("F-klav, linje 1", "bass1"),
-        BASS2("F-klav, linje 2", "bass2"),
-        BASS3("F-klav, linje 3", "bass3"),
-        BASS4("F-klav, linje 4", "bass4"),
-        BASS5("F-klav, linje 5", "bass5"),
+        BASS1("Basklav, linje 1", "bass1"),
+        BASS2("Basklav, linje 2", "bass2"),
+        BASS3("Basklav, linje 3", "bass3"),
+        BASS4("Basklav, linje 4", "bass4"),
+        BASS5("Basklav, linje 5", "bass5"),
         TENOR("C-klav, linje 4", "tenor"),
         SOPRANO("C-klav, linje 1", "alto1"),
         MEZZOSOPRANO("C-klav, linje 2", "alto2"),
-        BARITON("F-klav, linje 3", "bass3");
+        BARITON("Basklav, linje 3 (Baritonklav)", "bass3");
         private final String label;
         private final String code;
 
@@ -87,6 +87,15 @@ public interface Clef extends DomainEntity {
         public static Type getByCode(String code) {
             for (Type t : values()) {
                 if (t.getCode().equalsIgnoreCase(code)) {
+                    return t;
+                }
+            }
+            return DEFAULT_TYPE;
+        }
+
+        public static Type getByLabel(String label) {
+            for (Type t : values()) {
+                if (t.getLabel().equals(label)) {
                     return t;
                 }
             }
